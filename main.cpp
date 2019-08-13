910bc11c7d933601df4fa9f74a2f553372667853
Albert <grandia3@hotmail.com>
2019-08-13 22:19:59 +0700

    add load image

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <torch/script.h>
+/* #include <torch/script.h> */
 #include <memory>
 #include <iostream>
+#include "cimg/CImg.h"
+#include <opencv2/core/core.hpp>
 
 int main(int argc, const char* argv[])
 {
+    /*
 	if (argc != 2) {
     std::cerr << "usage: example-app <path-to-exported-script-module>\n";
     return -1;
@@ -22,4 +25,9 @@ int main(int argc, const char* argv[])
   }
 
   std::cout << "ok\n";
+   */
+   
+   //CImg<unsigned char> src("python_mnist_model/sample_tshirt.png");
+   
+   std::cout << "ok\n";
 }
